6978fcf566d1466c24a6270276b313dc0f5ee17e
priyamredker <redkerpriyam12@gmail.com>
2025-09-20 16:54:10 +0530

    Add files via upload

---
+++ Experiment 3/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.example.counterapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            counter++;
+            textView.setText("Count: " + counter);
+        });
+    }
+}
---
+++ Experiment 3/README.md
@@ -0,0 +1,7 @@
+# Experiment 3 - Android Studio Counter App
+
+This is a simple Counter App using Java in Android Studio.
+
+## How to Run
+1. Open in Android Studio.
+2. Run on Emulator/Device.
